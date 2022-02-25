467e7a657ba7175f171e012fba2d90151a4b98aa
Ian McConihay <mcconihay.ian92@gmail.com>
2022-02-25 09:28:12 -0600

    Hello

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Employee {
+	private long id;
+	private String name;
+	private int age;
+	private String dateJoined;
+
+	public Employee() {}
+
+	public Employee(long id, String name, int age, String dateJoined) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.dateJoined = dateJoined;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getDateJoined() {
+		return dateJoined;
+	}
+
+	public void setDateJoined(String dateJoined) {
+		this.dateJoined = dateJoined;
+	}
+
+
+
+
+
+}
